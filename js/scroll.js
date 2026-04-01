4b95af6cf633f53bc0135e9065b4739ce15bac4a
Sato <Yugo.Sato@sony.com>
2026-04-01 22:30:21 +0900

    proofread with copilot

--- js/scroll.js
+++ js/scroll.js
@@ -3,8 +3,13 @@ $(function() {
         var speed = 400;
         var href = $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
+        if (!target.length) {
+            return false;
+        }
         var position = target.offset().top;
-        var height = target.children('h4').height() * 2.5;
+        var heading = target.children('h4');
+        var headingHeight = heading.length ? heading.height() : 0;
+        var height = headingHeight * 2.5;
         $('body,html').animate({ scrollTop: position - height }, speed, 'swing');
         return false;
     });
